1c04719a98d4b6c4433151d4ca0c16b4d4112c17
luisfayre <luisfayre@hotmail.com>
2017-04-29 16:33:48 -0600

    Login - Register

---
+++ app/src/main/java/com/luisfayre/tecnologico/ProfileActivity.java
@@ -0,0 +1,35 @@
+package com.luisfayre.tecnologico;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    //Firebase
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener fireAutheListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if (user == null){
+            PantallaLogin();
+        }
+    }
+
+
+    private void PantallaLogin() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/luisfayre/tecnologico/RegisterActivity.java
+++ app/src/main/java/com/luisfayre/tecnologico/RegisterActivity.java
@@ -14,59 +14,48 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     //UI
-    private Button Login;
-    private EditText Email;
+    private Button Registro;
+    private EditText Correo;
     private EditText Contrasena;
 
-    //Firebase
     private FirebaseAuth firebaseAuth;
-    private FirebaseAuth.AuthStateListener fireAutheListener;
+    private FirebaseAuth.AuthStateListener fireAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
-        Login = (Button) findViewById(R.id.button_login);
-        Email = (EditText) findViewById(R.id.login_Email);
-        Contrasena = (EditText) findViewById(R.id.login_Contrasena);
+        Registro = (Button) findViewById(R.id.button_register);
+        Correo = (EditText) findViewById(R.id.register_email);
+        Contrasena = (EditText) findViewById(R.id.register_contraseña);
 
         firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-
-        if (user != null){
-            PantallaPrincipal();
-        }
-
     }
 
-    public void noRegistrado(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
-    }
-
-    public void login(View view) {
-        String email = Email.getText().toString().trim();
+    public void Registro(View view) {
+        String email = Correo.getText().toString().trim();
         String password = Contrasena.getText().toString().trim();
-        firebaseAuth.signInWithEmailAndPassword(email, password)
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "Logeado exitosamente", Toast.LENGTH_SHORT).show();
+                        if (task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Registro exitosamente", Toast.LENGTH_SHORT).show();
                             PantallaPrincipal();
-                        } else {
-                            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(RegisterActivity.this, "No se ha registrado correctamente", Toast.LENGTH_SHORT).show();
                         }
+
+
                     }
                 });
     }
-
     private void PantallaPrincipal() {
         Intent intent = new Intent(this, ProfileActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
